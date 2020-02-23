c9bbc1aa213b13fc2a7765b5b45869bbcb725b8c
Rattenfanger <995169650@qq.com>
2020-02-23 13:10:50 +0800

    test

---
+++ bpmdatamigrate/src/main/java/com/xjzhou/bpmdatamigrate/controller/ControllerTest.java
@@ -0,0 +1,6 @@
+package com.xjzhou.bpmdatamigrate.controller;
+public class ControllerTest{
+    public static void main(String args[]){
+        System.out.println("Hello world!");
+    }
+}
---
+++ bpmdatamigrate/src/test/java/com/xjzhou/bpmdatamigrate/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xjzhou.bpmdatamigrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
